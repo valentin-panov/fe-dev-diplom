9c23c236353a51a17766f8d64d32cee1c26a61ab
Valentin Panov <valentin.panov@me.com>
2021-11-05 22:00:58 +0300

    feat(mainPage) reviews implemented

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -3,6 +3,7 @@ import cn from 'clsx';
 import s from './LandingPage.module.scss';
 import { About } from '../About';
 import { HowItWorks } from '../HowItWorks';
+import { Reviews } from '../Reviews';
 
 export type Props = {
   className?: string;
@@ -12,5 +13,6 @@ export const LandingPage = memo<Props>(({ className }) => (
   <div className={cn(s.root, className)}>
     <About />
     <HowItWorks />
+    <Reviews />
   </div>
 ));
