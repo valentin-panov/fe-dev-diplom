9711ca3d0e4b837d884ca4981968b9f04cd5e338
Valentin Panov <valentin.panov@me.com>
2021-11-15 23:43:32 +0300

    fix(datePicker) month in RU and triangle added

--- src/components/Pickers/DatePickerOrigin/DatePickerOriginUnit.tsx
+++ src/components/Pickers/DatePickerOrigin/DatePickerOriginUnit.tsx
@@ -1,7 +1,15 @@
 import React, { memo } from 'react';
-import { DatePicker } from 'antd';
-import locale from 'antd/es/date-picker/locale/ru_RU';
+import { ConfigProvider, DatePicker } from 'antd';
+// eslint-disable-next-line camelcase
+import ru_RU from 'antd/lib/locale/ru_RU';
+
+import moment from 'moment';
+import 'moment/locale/ru';
+
+import cn from 'clsx';
 import s from './DatePickerOrigin.module.scss';
+
+moment.locale('ru');
 
 export type Props = {
   className?: string;
@@ -11,13 +19,15 @@ export type Props = {
 };
 
 export const DatePickerOriginUnit = memo<Props>(({ className, dateFormat, placeholder, getDate }) => (
-  <DatePicker
-    onChange={getDate}
-    className={s.inputItem}
-    locale={locale}
-    allowClear
-    format={dateFormat}
-    placeholder={placeholder}
-    dropdownClassName={className}
-  />
+  // eslint-disable-next-line camelcase
+  <ConfigProvider locale={ru_RU}>
+    <DatePicker
+      onChange={getDate}
+      className={cn(s.inputItem, className)}
+      allowClear
+      format={dateFormat}
+      placeholder={placeholder}
+      dropdownClassName={s.dropDown}
+    />
+  </ConfigProvider>
 ));
