a21b544fdbe09eef3c6e6ed4dbfde7328bc59afc
Valentin Panov <valentin.panov@me.com>
2021-11-22 21:56:01 +0300

    feat(SelectScreen TimeRange) antd slider implemented, formatted

--- src/components/SelectionScreen/SelectionFilterTime/SelectionFilterTime.tsx
+++ src/components/SelectionScreen/SelectionFilterTime/SelectionFilterTime.tsx
@@ -1,7 +1,6 @@
 import React, { memo, ReactElement, useState } from 'react';
 import cn from 'clsx';
-import InputRange, { Range } from 'react-input-range';
-import { Collapse } from 'antd';
+import { Collapse, Slider } from 'antd';
 
 import { ReactComponent as Plus } from '../svg/icon_dest_plus.svg';
 import { ReactComponent as Minus } from '../svg/icon_dest_minus.svg';
@@ -9,6 +8,7 @@ import { ReactComponent as Minus } from '../svg/icon_dest_minus.svg';
 import s from './SelectionFilterTime.module.scss';
 import 'react-input-range/lib/css/index.css';
 import './rewrite.css';
+import { Range } from '../SelectionFilterPrice';
 
 const { Panel } = Collapse;
 
@@ -19,42 +19,33 @@ export type Props = {
 };
 
 export const SelectionFilterTime = memo<Props>(({ className, icon, text }) => {
-  const [range, setRange] = useState<Range>({ min: 0, max: 1440 });
+  const [range, setRange] = useState<Range>([0, 1440]);
 
   const min = 0;
   const max = 1440;
 
-  const onChangeRange = (value: Range | number): void => {
+  const onChangeRange = (value: number | Range): void => {
     if (typeof value === 'number') {
       const minValue = 0;
       const maxValue = max > value ? value : max;
-      const setValue = { min: minValue, max: maxValue };
+      const setValue: Range = [minValue, maxValue];
       setRange(setValue);
     } else {
-      const minValue = min < value.min ? value.min : min;
-      const maxValue = max > value.max ? value.max : max;
-      const setValue = { min: minValue, max: maxValue };
+      const minValue = min < value[0] ? value[0] : min;
+      const maxValue = max > value[1] ? value[1] : max;
+      const setValue: Range = [minValue, maxValue];
       setRange(setValue);
     }
   };
 
-  // const min2hour = (value: Range) => {
-  //   let hours1 = Math.floor(value.min / 60);
-  //   let minutes1 = value.min[0] - hours1 * 60;
-  //
-  //   if (hours1.length < 10) hours1 = `0${hours}`;
-  //   if (minutes1.length < 10) minutes1 = `0${minutes}`;
-  //
-  //   if (minutes1 === 0) minutes1 = '00';
-  //
-  //   let hours2 = Math.floor(value.max / 60);
-  //   let minutes2 = value.max - hours2 * 60;
-  //
-  //   if (hours2.length < 10) hours2 = `0${hours}`;
-  //   if (minutes2.length < 10) minutes2 = `0${minutes}`;
-  //
-  //   if (minutes2 === 0) minutes2 = '00';
-  // };
+  const formatterDuration = (value: number | undefined): string => {
+    if (typeof value === 'undefined') {
+      return '';
+    }
+    const hour = Math.floor(value / 60);
+    const minutes = value - hour * 60;
+    return `${`0${hour}`.slice(-2)}:${`0${minutes}`.slice(-2)}`;
+  };
 
   return (
     <div className={cn(s.root, className)}>
@@ -70,15 +61,28 @@ export const SelectionFilterTime = memo<Props>(({ className, icon, text }) => {
         >
           <div className={s.timePickerPanel}>
             <div className={cn(s.timePickerSubTitle, s.firstST)}>Время отбытия</div>
-
+            <Slider
+              max={max}
+              min={min}
+              range={{ draggableTrack: true }}
+              step={30}
+              defaultValue={range}
+              tooltipVisible
+              tooltipPlacement="bottom"
+              tipFormatter={(value) => formatterDuration(value)}
+              onChange={(value: number | Range) => onChangeRange(value)}
+            />
             <div className={cn(s.timePickerSubTitle, s.secondST)}>Время прибытия</div>
-            <InputRange
-              maxValue={max}
-              minValue={min}
-              value={range}
-              onChange={(value) => onChangeRange(value)}
-              draggableTrack
-              step={15}
+            <Slider
+              max={max}
+              min={min}
+              range={{ draggableTrack: true }}
+              step={30}
+              defaultValue={range}
+              tooltipVisible
+              tooltipPlacement="bottom"
+              tipFormatter={(value) => formatterDuration(value)}
+              onChange={(value: number | Range) => onChangeRange(value)}
             />
           </div>
         </Panel>
