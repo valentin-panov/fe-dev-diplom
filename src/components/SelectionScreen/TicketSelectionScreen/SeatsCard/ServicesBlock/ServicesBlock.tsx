cb24c9d8ea7cd129c35bddd40fb0d2cae7f0c73b
Valentin Panov <valentin.panov@me.com>
2022-02-07 00:40:48 +0300

    feat(src/components/SelectionScreen/TicketSelectionScreen/SeatsCard/ServicesBlock/ServicesBlock.tsx) some user exp improvements

--- src/components/SelectionScreen/TicketSelectionScreen/SeatsCard/ServicesBlock/ServicesBlock.tsx
+++ src/components/SelectionScreen/TicketSelectionScreen/SeatsCard/ServicesBlock/ServicesBlock.tsx
@@ -12,14 +12,27 @@ export type Props = {
 
 export const ServicesBlock = memo<Props>(({ data }) => {
   const { have_air_conditioning: ac, have_wifi: wifi, is_linens_included: linensInc } = data.coach;
+  // console.log(data);
   return (
     <div className={s.sciBlock}>
       <div>Обслуживание ФПК</div>
       <div className={s.sciBlockServices}>
-        {ac && <div className={s.serviceIcon}>{filtersCollection.have_air_conditioning.element}</div>}
-        {wifi && <div className={s.serviceIcon}>{filtersCollection.have_wifi.element}</div>}
-        <div className={cn(s.serviceIcon, linensInc ? s.included : '')}>{filtersCollection.linen.element}</div>
-        <div className={cn(s.serviceIcon)}>{filtersCollection.cup.element}</div>
+        {ac && (
+          <button type="button" disabled className={cn(s.serviceIcon)}>
+            {filtersCollection.have_air_conditioning.element}
+          </button>
+        )}
+        {wifi && (
+          <button type="button" className={s.serviceIcon}>
+            {filtersCollection.have_wifi.element}
+          </button>
+        )}
+        <button type="button" disabled={linensInc} className={cn(s.serviceIcon, linensInc ? s.included : '')}>
+          {filtersCollection.linen.element}
+        </button>
+        <button type="button" disabled className={cn(s.serviceIcon)}>
+          {filtersCollection.cup.element}
+        </button>
       </div>
     </div>
   );
